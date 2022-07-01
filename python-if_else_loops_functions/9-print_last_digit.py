7131656038afcfdde10ca7afb97799b683405d10
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-01 07:32:20 -0700

    9-print_last_digit.py updated

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        dgt = number % (-10)
-    else:
-        dgt = number % 10
-    return dgt
+        number = number * -1
+    print('{}'.format(number % 10), end='')
+    return (number % 10)
