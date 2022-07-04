5dbcd107b3ba83838baba9d6015688bf60756556
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-04 04:42:11 -0700

    100-my_calculator.py updated

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("Usage: argv[0] <a> <operator> <b>")
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
 
     opr = argv[2]
