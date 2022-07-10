e77745688983bb288e227f3e828c0204dccdb272
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-10 07:26:14 -0700

    7-add_tuple.py updated

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     ade = len(tuple_a)
     bisi = len(tuple_b)
 
-    sum1 = (tuple_a[0] if ade > 0 else 0) + (tuple_b[0] if bisi > 0 else)
-    sum2 = (tuple_a[1] if ade > 1 else 0) + (tuple_b[1] if bisi > 1 else)
+    sum1 = (tuple_a[0] if ade > 0 else 0) + (tuple_b[0] if bisi > 0 else 0)
+    sum2 = (tuple_a[1] if ade > 1 else 0) + (tuple_b[1] if bisi > 1 else 0)
 
     return (sum1, sum2)
