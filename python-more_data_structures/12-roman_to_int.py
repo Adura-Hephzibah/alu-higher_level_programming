938de94de59bb80d4c13b99e4921b044187698f7
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-11 14:35:16 -0700

    12-roman_to_int.py updated

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if (type(roman_string) is not str) or roman_string is None:
         return 0
-    if if type(roman_string) is str:
+    if type(roman_string) is str:
         nar = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         add = 0
         for x, y in zip(roman_string, roman_string[1:]):
