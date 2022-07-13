193a0279c419eebddb0c9d0d2efada8312810a97
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-13 13:21:56 -0700

    102-complex_delete.py updated

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,5 +2,5 @@
 def complex_delete(a_dictionary, value):
     for key in a_dictionary.keys():
         if a_dictionary[key] == value:
-            del key
+            del a_dictionary[key]
     return a_dictionary
