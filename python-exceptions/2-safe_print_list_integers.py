87dd8c87738909b835e812c095eabdcbd4ea7b69
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-18 01:54:04 -0700

    2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idex = 0
+    while idex < 0:
+        try:
+            print("{:d}".format(my_list[idex]), end='')
+        except(TypeError, ValueError):
+            continue
+        idex += 1
+    print()
+    return idex
