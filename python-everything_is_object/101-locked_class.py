52615289dad507c2a9127c6d132220d1608eb47f
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-25 22:40:06 +0200

    update

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-class LockedClass:
-    """ 
-     prevents the user from dynamically creating new instance attributes
-     except if the new instance attribute is called first_name
-     """
+"""
+locked class that prevents dynamically creating
+instance attributes
+"""
 
-     __slots__ = 'first_name'
+
+class LockedClass:
+    """prevent dynamically created attributes"""
+    __slots__ = "first_name"
