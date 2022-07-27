9dcb21d486685a3b7d91b42193f6bebca157d6c5
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-27 22:49:07 +0200

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 1-write_file.py
+
+function writes a string to text file, returns number of char
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to text file, returns number of char"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return (f.write(text))
