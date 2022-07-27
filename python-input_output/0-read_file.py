95475eafd9bdfb5944c7dbb9db24e2d3ed383e6d
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-27 22:33:10 +0200

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0-read_file.py
+
+contains function that reads a text file and prints
+"""
+
+
+def read_file(filename=""):
+    """read and print text from file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read())
