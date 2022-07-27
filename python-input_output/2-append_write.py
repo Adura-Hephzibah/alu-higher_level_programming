c543eb72cc2b4b2273eb99301607617c91734eee
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-27 22:55:42 +0200

    update

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,5 +8,6 @@ function appends a string to text file, returns number of char
 
 def append_file(filename="", text=""):
     """writes a string to text file, returns number of char"""
+
     with open(filename, mode="a", encoding="utf-8") as f:
         return (f.write(text))
