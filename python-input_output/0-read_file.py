f78beeb3acf8d5d39149d32f4d58a42dcb1be1d3
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-27 22:36:33 +0200

    added end=""

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ contains function that reads a text file and prints
 def read_file(filename=""):
     """read and print text from file"""
     with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
