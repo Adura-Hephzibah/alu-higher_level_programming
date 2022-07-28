42251e4e7fb09db56c8d859abebf447855a4ca0d
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-28 12:18:53 +0200

    100-append_after.py updated

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -12,6 +12,6 @@ def append_after(filename="", search_string="", new_string=""):
         for line in f:
             data += line
             if search_string in line:
-                data += new_strings
+                data += new_string
         f.seek(0)
         f.write(data)
