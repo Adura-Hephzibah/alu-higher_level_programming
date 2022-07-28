de807581397eb534cb448e9a64b265e490b467f1
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-07-28 11:33:29 +0200

    12-pascal_triangle.py updated

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -20,6 +20,6 @@ def pascal_triangle(n):
 
     tri = [[1]]
     for row in range(n-1):
-        triangle.append([i+j for i, j in
-                        zip([0]+triangle[-1], triangle[-1] + [0])])
-    return triangle
+        tri.append([i+j for i, j in
+                   zip([0]+triangle[-1], triangle[-1] + [0])])
+    return tri
