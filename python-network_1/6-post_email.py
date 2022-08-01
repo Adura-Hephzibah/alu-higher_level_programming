bc7529d8304ea56c3147f2968c44afb8fb9e7481
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-01 17:48:21 +0200

    6-post_email.py

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 6-post_email.py
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.post(argv[1], data={"email": argv[2]})
+    print(r.text)
