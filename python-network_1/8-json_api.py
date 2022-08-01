0f7d058a2514e6b9c97d237d92a89841b238c5aa
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-01 18:16:36 +0200

    8-json_api.py

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module 8-json_api.py
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        letter = ""
+    else:
+        letter = argv[1]
+    dic = {"q": letter}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=dic)
+    dic_1 = r.json()
+    try:
+        if dic_1:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
