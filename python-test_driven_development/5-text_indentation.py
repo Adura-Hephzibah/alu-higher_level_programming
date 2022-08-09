1115f10ad0de9d51df48a9cc60c823512bc99e1d
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-09 10:32:04 +0200

    5-text_indentation.py adjusted

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,10 +10,11 @@ def text_indentation(text):
     """
     Prints text with 2 new lines after each ".", "?", and ":"
     """
-
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     for char in ".?:":
         text = text.replace(char, char + "\n\n")
-    new = text.split(\n)
+    new = text.split('\n')
     for line in new:
         item = line.strip(' ')
         print(item)
