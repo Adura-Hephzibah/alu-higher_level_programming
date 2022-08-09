80764694405d03c13cd41c8a8f9dfe69ecfa6d5f
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-09 08:36:12 +0200

    4-print_square.py updated

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -14,5 +14,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    print('\n'.join('#'*size for n in range(size)))
+    if size == 0:
+        print("", end="")
+    else:
+        print('\n'.join('#'*size for n in range(size)))
