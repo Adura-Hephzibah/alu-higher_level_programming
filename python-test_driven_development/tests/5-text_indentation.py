bba7938bf87aa8cb7e076e68bd110b0f733ef1d8
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-09 10:25:36 +0200

    5-text_indentation.py

---
+++ python-test_driven_development/tests/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 5-text_indentation.py
+function that prints a text with 2 new lines after each of
+these characters: ".", "?" and ":"
+"""
+
+
+def text_indentation(text):
+    """
+    Prints text with 2 new lines after each ".", "?", and ":"
+    """
+
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    new = text.split(\n)
+    for line in new:
+        item = line.strip(' ')
+        print(item)
