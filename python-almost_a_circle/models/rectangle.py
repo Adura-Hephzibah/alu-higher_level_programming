dcc82ec0671aacf23803c39e1dbeacceb78f00ad
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-15 16:02:59 +0200

    rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+contains class Rectangle
+inherits from Base
+"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class definition"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """getter height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """getter x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """getter y"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """setter width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setter height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """setter x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """setter y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
