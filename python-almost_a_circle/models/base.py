b9e5d8ab32e9438dc47dddc2f56e2950f039211a
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-16 17:57:30 +0200

    hmmm

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,8 +22,8 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """documented"""
+        """Return a JSON string representation"""
         if list_dictionaries is None:
             return "[]"
         else:
-            return json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries))
