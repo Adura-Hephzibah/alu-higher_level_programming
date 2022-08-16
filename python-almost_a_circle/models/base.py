ab6f2011511d687e543309df75104c01ba3c133d
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-16 17:32:37 +0200

    what is happenning

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,5 +24,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """documented"""
         if list_dictionaries is None:
-            list_dictionaries = []
-        return json.dumps(list_dictionaries)
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
