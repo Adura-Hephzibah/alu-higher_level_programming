2463ec3e05f593f5f36b59eaadf62ed980004b1b
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-16 18:00:34 +0200

    hmmmmm

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,4 +26,4 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         else:
-            return json.dumps(list_dictionaries))
+            return json.dumps(list_dictionaries)
