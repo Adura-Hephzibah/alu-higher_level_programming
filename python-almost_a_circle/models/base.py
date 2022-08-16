a9b043d8d5546dd0cf6645e09e04d99d95283efb
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-16 22:54:43 +0200

    fresh start

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module base.py
+"""
+
+
+import json
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return a JSON string representation"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
