f836c5de7c3cdbfabeff55f57fe5417a5aeec6d4
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-18 19:46:15 +0200

    another commit

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,7 @@ Unittest for class Base
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -65,3 +66,66 @@ class TestBase(unittest.TestCase):
         j_ls = Base.from_json_string(j)
         self.assertEqual(j_ls, [])
         self.assertTrue(isinstance(j_ls, list))
+
+
+class TestRectangle(unittest.TestCase):
+    """Tests"""
+
+    def test_rec(self):
+        """test"""
+        r1 = Rectangle(20, 30, 2, 3, 1)
+        r2 = Rectangle(3, 4)
+        self.assertEqual(r1.width, 20)
+        self.assertEqual(r1.height, 30)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 3)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.width, 3)
+        self.assertEqual(r2.height, 4)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertTrue(r2.id, not None)
+
+    def test_validation(self):
+        """test"""
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+            Rectangle(1, "2")
+            Rectangle(1, 2, "3")
+            Rectangle(1, 2, 3, "4")
+
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2, 3, 4)
+            Rectangle(1, -2, 3, 4)
+            Rectangle(1, 2, -3, 4)
+            Rectangle(1, 2, 3, -4)
+
+    def test_area_str_display(self):
+        """test"""
+        r3 = Rectangle(3, 4)
+        self.assertEqual(r3.area(), 12)
+        self.assertEqual(str(r3), '[Rectangle] (1) 0/0 - 3/4')
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3", 4)
+
+    def test_create(self):
+        """tests"""
+        """returns an instance with all attributes already set"""
+        dic = {"width": 3, "height": 4}
+        rect = Rectangle.create(**dic)
+        self.assertEqual(rect.area(), 12)
+
+    def test_to_dictionary(self):
+        """tests"""
+        r4 = Rectangle(3, 4, 2, 5, 1)
+        self.assertEqual(r4.to_dictionary(),
+                         {"id": 1, "width": 3, "height": 4, "x": 2, "y": 5})
+
+    def test_update(self):
+        """tests"""
+        r = Rectangle(3, 4,)
+        r.update(width=4, height=5, x=2, y=3, id=15)
+        self.assertEqual(r.area(), 20)
+        dic = {'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4}
+        r.update(**dic)
+        self.assertEqual(r.area(), 2)
