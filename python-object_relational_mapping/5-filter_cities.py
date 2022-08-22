7e7eb36c5299fafae69f459464bba5390062a31d
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-08-22 23:27:48 +0200

    updated

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     cur.execute("""SELECT cities.name
                 FROM cities LEFT JOIN states
                 ON cities.state_id = states.id
-                WHERE cities.name = %s ORDER BY cities.id ASC""", (argv[4],))
+                WHERE states.name = %s ORDER BY cities.id ASC""", (argv[4], ))
     query_rows = cur.fetchall()
-    print(", ".join([row[1] for row in query_rows]))
+    print(", ".join([row[0] for row in query_rows]))
     cur.close()
     conn.close()
