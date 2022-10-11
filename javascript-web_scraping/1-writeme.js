d52a6e7c312d088be4ca9cfdee7ba98c050eccb0
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-10-11 11:04:32 +0200

    Write me

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, 'utf-8', (error) => console.error(error));
